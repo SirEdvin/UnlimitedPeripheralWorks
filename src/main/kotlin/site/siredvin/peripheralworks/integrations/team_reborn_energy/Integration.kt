716eed849b677c46946094c836b84896a9f0e887
SirEdvin <siredvin.dark@gmail.com>
2022-05-08 17:12:39 +0300

    Add ability for turtle to use tech reborn batteries (and many others) as fuel level

--- src/main/kotlin/site/siredvin/peripheralworks/integrations/team_reborn_energy/Integration.kt
+++ src/main/kotlin/site/siredvin/peripheralworks/integrations/team_reborn_energy/Integration.kt
@@ -1,9 +1,11 @@
 package site.siredvin.peripheralworks.integrations.team_reborn_energy
 
+import dan200.computercraft.api.turtle.event.TurtleEvent
 import site.siredvin.peripheralworks.computercraft.ComputerCraftProxy
 
 class Integration: Runnable {
     override fun run() {
         ComputerCraftProxy.addProvider(EnergyStoragePlugin.Provider())
+        TurtleEvent.EVENT_BUS.register(EnergyRefuelHandler)
     }
 }
