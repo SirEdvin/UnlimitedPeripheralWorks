eca59036ee7464fa5eca2d447af68753b9cc9c22
siredvin <siredvin.dark@gmail.com>
2023-05-28 22:27:20 +0300

    feat(gradle): migrate to build plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,76 +1,19 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.kotlin)
+    id("site.siredvin.root") version "0.3.5"
 }
 
-val mavenGroup by properties
-fun getenv(path: String = ".env"): Map<String, String> {
-    val env = hashMapOf<String, String>()
-
-    val file = File(path)
-    if (file.exists()) {
-        file.readLines().forEach { line ->
-            val splitResult = line.split("=")
-            if (splitResult.size > 1) {
-                env[splitResult[0].trim()] = splitResult[1].trim()
-            }
-        }
-    }
-
-    return env
+subprojectShaking {
+    withKotlin.set(true)
 }
 
-val secretEnv = getenv()
-val rootProjectDir = projectDir
+val setupSubproject = subprojectShaking::setupSubproject
 
 
 subprojects {
-    apply(plugin = "kotlin")
-    apply(plugin = "maven-publish")
-
-    group = mavenGroup!!
-    this.extra["curseforgeKey"] = secretEnv["CURSEFORGE_KEY"] ?: System.getenv("CURSEFORGE_KEY") ?: ""
-    this.extra["modrinthKey"] = secretEnv["MODRINTH_KEY"] ?: System.getenv("MODRINTH_KEY") ?: ""
-    this.extra["rootProjectDir"] = rootProjectDir
-
-    val javaVersion = JavaVersion.VERSION_17
-    java {
-        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-        withSourcesJar()
-    }
-    tasks {
-        withType<JavaCompile> {
-            options.encoding = "UTF-8"
-            sourceCompatibility = javaVersion.toString()
-            targetCompatibility = javaVersion.toString()
-            options.release.set(javaVersion.toString().toInt())
-        }
-        withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-            kotlinOptions { jvmTarget = javaVersion.toString() }
-        }
-    }
+    setupSubproject(this)
 }
 
 repositories {
     mavenCentral()
 }
-
-//idea.project.settings.compiler.javac {
-//    // We want ErrorProne to be present when compiling via IntelliJ, as it offers some helpful warnings
-//    // and errors. Loop through our source sets and find the appropriate flags.
-//    moduleJavacAdditionalOptions = subprojects
-//        .asSequence()
-//        .map { evaluationDependsOn(it.path) }
-//        .flatMap { project ->
-//            val sourceSets = project.extensions.findByType(SourceSetContainer::class) ?: return@flatMap sequenceOf()
-//            sourceSets.asSequence().map { sourceSet ->
-//                val name = "${idea.project.name}.${project.name}.${sourceSet.name}"
-//                val compile = project.tasks.named(sourceSet.compileJavaTaskName, JavaCompile::class).get()
-//                name to compile.options.allCompilerArgs.joinToString(" ") { if (it.contains(" ")) "\"$it\"" else it }
-//            }
-//
-//        }
-//        .toMap()
-//}
--- projects/core/build.gradle.kts
+++ projects/core/build.gradle.kts
@@ -1,50 +1,26 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.vanillaGradle)
-    alias(libs.plugins.kotlin)
-    idea
+    id("site.siredvin.vanilla")
+    id("site.siredvin.publishing")
 }
 
 val modVersion: String by extra
 val minecraftVersion: String by extra
 val modBaseName: String by extra
 
-base {
-    archivesName.set("${modBaseName}-common-${minecraftVersion}")
-    version = modVersion
+baseShaking {
+    projectPart.set("common")
+    shake()
 }
 
-sourceSets.main.configure {
-    resources.srcDir("src/generated/resources")
-}
-
-minecraft {
-    version(minecraftVersion)
-    accessWideners(
-        "src/main/resources/peripheralworks.accesswidener",
-        "src/main/resources/peripheralworks-common.accesswidener"
-    )
+vanillaShaking {
+    accessWideners.add("src/main/resources/peripheralworks-common.accesswidener")
+    accessWideners.add("src/main/resources/peripheralworks.accesswidener")
+    shake()
 }
 
 repositories {
-    mavenCentral()
     mavenLocal()
-    // For CC:T common code
-    maven {
-        url = uri("https://squiddev.cc/maven/")
-        content {
-            includeGroup("cc.tweaked")
-            includeModule("org.squiddev", "Cobalt")
-        }
-    }
-    // For Forge configuration common code
-    maven {
-        name = "Fuzs Mod Resources"
-        url = uri("https://raw.githubusercontent.com/Fuzss/modresources/main/maven/")
-        content {
-            includeGroup("fuzs.forgeconfigapiport")
-        }
-    }
 }
 
 
@@ -53,16 +29,4 @@ dependencies {
     implementation(libs.bundles.cccommon)
     api(libs.bundles.apicommon)
     compileOnly(libs.mixin)
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            artifactId = base.archivesName.get()
-            from(components["java"])
-        }
-    }
-    repositories {
-        mavenLocal()
-    }
 }
