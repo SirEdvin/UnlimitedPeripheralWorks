29779594ce3e2689700ca6991b4d0e694e75f708
SirEdvin <me@siredvin.site>
2025-11-27 19:47:31 +0200

    style: correct everything

--- projects/forge/src/main/kotlin/site/siredvin/peripheralworks/ForgePeripheralWorksClient.kt
+++ projects/forge/src/main/kotlin/site/siredvin/peripheralworks/ForgePeripheralWorksClient.kt
@@ -3,16 +3,13 @@ package site.siredvin.peripheralworks
 import dan200.computercraft.api.client.turtle.RegisterTurtleModellersEvent
 import dan200.computercraft.api.turtle.ITurtleUpgrade
 import dan200.computercraft.api.turtle.TurtleUpgradeSerialiser
-import net.minecraft.client.Minecraft
 import net.minecraft.resources.ResourceLocation
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.client.event.EntityRenderersEvent.RegisterRenderers
 import net.minecraftforge.client.event.ModelEvent.RegisterAdditional
-import net.minecraftforge.client.event.RenderLevelStageEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
-import site.siredvin.peripheralworks.client.configurator.ConfigurationModeRenderRegistry
 
 @Mod.EventBusSubscriber(modid = PeripheralWorksCore.MOD_ID, value = [Dist.CLIENT], bus = Mod.EventBusSubscriber.Bus.MOD)
 object ForgePeripheralWorksClient {
