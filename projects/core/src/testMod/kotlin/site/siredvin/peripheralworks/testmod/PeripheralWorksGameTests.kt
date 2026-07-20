f2b98da21e834104a163a3fc6bb0adf27f374f50
SirEdvin <me@siredvin.site>
2026-07-20 09:25:21 +0000

    test: exercise core peripheral methods

--- projects/core/src/testMod/kotlin/site/siredvin/peripheralworks/testmod/PeripheralWorksGameTests.kt
+++ projects/core/src/testMod/kotlin/site/siredvin/peripheralworks/testmod/PeripheralWorksGameTests.kt
@@ -7,45 +7,45 @@ import site.siredvin.testiarium.cct.thenLua
 
 @TestGroup("peripheralworks")
 class PeripheralWorksGameTests {
-    @GameTest(template = "peripheralworksgametests.universal_scanner", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.universal_scanner", timeoutTicks = 2400)
     fun universalScanner(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.ultimate_sensor", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.ultimate_sensor", timeoutTicks = 2400)
     fun ultimateSensor(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.item_pedestal", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.item_pedestal", timeoutTicks = 2400)
     fun itemPedestal(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.map_pedestal", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.map_pedestal", timeoutTicks = 2400)
     fun mapPedestal(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.display_pedestal", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.display_pedestal", timeoutTicks = 2400)
     fun displayPedestal(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.remote_observer", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.remote_observer", timeoutTicks = 2400)
     fun remoteObserver(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.peripheral_proxy", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.peripheral_proxy", timeoutTicks = 2400)
     fun peripheralProxy(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.reality_forger", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.reality_forger", timeoutTicks = 2400)
     fun realityForger(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.recipe_registry", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.recipe_registry", timeoutTicks = 2400)
     fun recipeRegistry(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.informative_registry", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.informative_registry", timeoutTicks = 2400)
     fun informativeRegistry(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.statue_workbench", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.statue_workbench", timeoutTicks = 2400)
     fun statueWorkbench(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.entity_link", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.entity_link", timeoutTicks = 2400)
     fun entityLink(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.network_manager", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.network_manager", timeoutTicks = 2400)
     fun networkManager(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 
-    @GameTest(template = "peripheralworksgametests.hologram_projector", timeoutTicks = 1200)
+    @GameTest(template = "peripheralworksgametests.hologram_projector", timeoutTicks = 2400)
     fun hologramProjector(helper: GameTestHelper) = helper.thenLua().thenSucceed()
 }
