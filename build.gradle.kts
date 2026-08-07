ec593305a4608694dce095da50efd6165ee06e14
SirEdvin <me@siredvin.site>
2026-08-07 22:13:35 +0000

    fix: update CurseForge publishing metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.loom) apply false
-    id("site.siredvin.root") version "0.9.0"
-    id("site.siredvin.release") version "0.9.0"
+    id("site.siredvin.root") version "0.9.1"
+    id("site.siredvin.release") version "0.9.1"
     id("com.dorongold.task-tree") version "4.0.0"
     id("com.github.ben-manes.versions") version "0.51.0"
 }
